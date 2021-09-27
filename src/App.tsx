1e8e7a8e32a15d0fc4422796fb023174f60a965a
Vasiliki Pasalidi-Chantzi <van3pasa@hotmail.com>
2021-09-27 16:15:52 +0100

    Run lint and format

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
 import { PastebinForm } from "./components/PastebinForm";
 
-
 function App(): JSX.Element {
-
   const [paste, setPaste] = useState<Array<string>>([]);
   return (
     <>
-    <PastebinForm paste={paste} setPaste={setPaste}/>
+      <PastebinForm paste={paste} setPaste={setPaste} />
     </>
   );
 }
--- src/components/PastebinForm.tsx
+++ src/components/PastebinForm.tsx
@@ -4,39 +4,42 @@ import { useState } from "react";
 config();
 const apiBaseURL = process.env.REACT_APP_API_BASE;
 
-export function PastebinForm({paste, setPaste}:{paste: string[]; setPaste: React.Dispatch<React.SetStateAction<string[]>>}):JSX.Element{
-
+export function PastebinForm({
+  paste,
+  setPaste,
+}: {
+  paste: string[];
+  setPaste: React.Dispatch<React.SetStateAction<string[]>>;
+}): JSX.Element {
   const [text, setText] = useState("");
-  const onFormSubmit = async (event: React.FormEvent<HTMLFormElement>) =>{
+  const onFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     try {
       setPaste([...paste, text]);
-      const content = paste[-1]
+      const content = paste[-1];
       const response = await fetch(apiBaseURL + "", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(content)
+        body: JSON.stringify(content),
       });
       // window.location ="/";
     } catch (error) {
-      console.error(error.message)      
+      console.error(error.message);
     }
   };
 
-    return (
+  return (
     <form className="pastebin-form" onSubmit={onFormSubmit}>
-        <input 
-          type="text" 
-          value={text}
-          className="pastebin-input"
-          placeholder="Paste your text here."
-          onChange ={ e => setText(e.target.value)}
-          />
-          <button 
-          className="submit-form-btn"
-          onClick={()=>setText("")}>
-            Save Paste
-          </button>
+      <input
+        type="text"
+        value={text}
+        className="pastebin-input"
+        placeholder="Paste your text here."
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button className="submit-form-btn" onClick={() => setText("")}>
+        Save Paste
+      </button>
     </form>
-    );
+  );
 }
